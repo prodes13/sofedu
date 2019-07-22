eec3d37a5879750c0aa86d02b0c2b9b8d22ed2bd
prodes13 <ciobotaru.florin@gmail.com>
2019-07-22 10:25:09 +0300

    minor modifications made

--- src/componente/Teste/Test1.js
+++ src/componente/Teste/Test1.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Test1.css';
 
-const Test1 = () => {
+class Test1 extends Component {
+    state = {
+        metri1: '',
+
+    }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+render() {
     return (
         <>
             <section className ="container">
                 <form>
                     <h2 className="mt-4 mb-4">Completati spatiile libere:</h2>
                     <p>
-                        1 km = 1000 <input className="c-putine" type="text" required/> = 10 <input className="c-putine" type="text" required/> = 100 <input className="c-putine" type="text" required/>
+                        1 km = 1000 <input className="c-putine" value={this.state.metri1} type="text" name="metri1" onChange={this.handleChange} required/> = 10 <input className="c-putine" type="text" required/> = 100 <input className="c-putine" type="text" required/>
                     </p>
                     <p>
                         800 cm = 80 <input className="c-putine" type="text" required/> = 8000 <input className="c-putine" type="text" required/> = 8 <input className="c-putine" type="text" required/>
@@ -26,12 +37,13 @@ const Test1 = () => {
                         <input className="c-putine" type="text" required/> dam = <input className="c-putine" type="text" required/> dm = <input className="c-putine" type="text" required/> hm = 500 m
                     </p>
                     
-                    <button type="button" class="btn btn-secondary">Resetare</button>
-                    <button type="submit" class="btn btn-primary ml-4">Gata!</button>
+                    <button type="button" className="btn btn-secondary">Resetare</button>
+                    <button type="submit" className="btn btn-primary ml-4">Gata!</button>
                 </form>
             </section>
         </>
-    )
+     )
+    }
 }
 
 export default Test1
